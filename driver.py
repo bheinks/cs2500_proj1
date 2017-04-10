00e6609bb052d79f3b302b9eb15072a0eb0df7c7
Brett Heinkel <bheinks@gmail.com>
2017-04-10 17:32:01 -0500

    code appears to be working. time to make some graphs

--- driver.py
+++ driver.py
@@ -1,19 +1,80 @@
 #!/usr/bin/env python3
 
 from sensornetwork import SensorNetwork
-from random import uniform
-from pprint import pprint
+from time import sleep
 
-sn = SensorNetwork(50, 50, 5, 300, 1000, False)
+sn = SensorNetwork(50, 100, 5, 300, 1000, True)
 
-#print(len(sn.find_intersections()))
+## random top down
+#rounds = 0
+#cov = 100
+#while cov > 50:
+#    print(len(sn.random_top_down()))
+#    cov = sn.coverage()
+#    print("Coverage is {:.2f}%".format(cov))
+#    rounds += 1
+#print(len(sn.random_top_down()))
+#print("random top down -", rounds)
+#sleep(5)
 
-#for sensor in sn.sensors:
-#    print("Coverage is {:.2f}%".format(sn.coverage()))
-#    print(sn.is_redundant(sensor))
 
-#print(sum(s.is_active for s in sn.sensors), len(sn.sensors))
 
-init_coverage = sn.coverage()
+# random bottom up
+print("="*24)
+sn.reset(False)
+initial = True
+#measuring = False
+rounds = 0
+cov = 0
+max_cov = 0
+while True:
+    if initial and cov > 50:
+        rounds = 0
+        initial = False
 
-while sn.coverage()
+    if not initial and cov <= 50:
+        break
+
+    print(len(sn.random_bottom_up()))
+    cov = sn.coverage()
+    print("Coverage is {:.2f}%".format(cov))
+    rounds += 1
+
+#    if cov >= max_cov:
+#        max_cov = cov
+#    elif cov < max_cov and not measuring:
+#        print(max_cov)
+#        rounds = 0
+#        measuring = True
+
+print("random bottom up -", rounds)
+sleep(5)
+
+
+
+# all_active
+print("="*24)
+sn.reset(True)
+rounds = 0
+cov = 100
+while cov > 50:
+    print(len(sn.all_active()))
+    cov = sn.coverage()
+    print("Coverage is {:.2f}%".format(cov))
+    rounds += 1
+print("all active -", rounds)
+sleep(5)
+
+
+
+# greedy top down
+sn.reset(True)
+rounds = 0
+cov = 100
+sn.init_greedy()
+while cov > 50:
+    print(len(sn.greedy_top_down()))
+    cov = sn.coverage()
+    print("Coverage is {:.2f}%".format(cov))
+    rounds += 1
+print("greedy top down -", rounds)
--- sensornetwork.py
+++ sensornetwork.py
@@ -1,16 +1,20 @@
+import random
+
 from sensor import Sensor
-from random import uniform, choice
 from pprint import pprint
 
 class SensorNetwork:
-    def __init__(self, aoi_size, num_sensors, r, e, num_samples, is_active):
+    def __init__(self, aoi_size, num_sensors, r, init_e, num_samples, is_active):
         # set of randomly positioned sensors arranged within AoI
-        self.sensors = [Sensor(uniform(0, 50), uniform(0, 50), r, e, is_active)
+        self.sensors = [Sensor(random.uniform(0, 50), random.uniform(0, 50), r, init_e, is_active)
                 for i in range(num_sensors)]
+        self.redundant_sensors = []
+        self.a_sensors = []
+        self.init_e = init_e
 
         # set of randomly selected points in AoI to determine coverage via monte
         # carlo method
-        self.mc_points = [(uniform(1, aoi_size-1), uniform(1, aoi_size-1))
+        self.mc_points = [(random.uniform(1, aoi_size-1), random.uniform(1, aoi_size-1))
                 for i in range(num_samples)]
 
     # determine coverage using monte carlo points
@@ -20,17 +24,17 @@ class SensorNetwork:
 
     def is_covered(self, x, y, only_active = False):
         for sensor in self.sensors:
-            if sensor.is_alive and sensor.contains_point(x, y):
+            if sensor.is_alive and sensor.is_active and sensor.contains_point(x, y):
                 return True
         return False
 
     # find and return a list of all intersection points between sensors
-    def find_intersections(self):
+    def find_intersections(self, sensors):
         intersections = []
 
-        for i, s1 in enumerate(self.sensors):
-            for s2 in self.sensors[i+1:]:
-                i1, i2 = s1.intersects2(s2)
+        for i, s1 in enumerate(sensors):
+            for s2 in sensors[i+1:]:
+                i1, i2 = s1.intersects(s2)
 
                 if i1 and i1 not in intersections:
                     intersections.append(i1)
@@ -38,7 +42,8 @@ class SensorNetwork:
                     intersections.append(i2)
         
         # filter intersections for only points that are within sensor range
-        return [(x, y) for x, y in intersections if self.is_covered(x, y)]
+        #return [(x, y) for x, y in intersections if self.is_covered(x, y)]
+        return intersections
 
     def is_redundant(self, sensor):
         # find the intersection points that are covered by this sensor
@@ -47,12 +52,12 @@ class SensorNetwork:
 
         # temporarily disable sensor to determine the difference in coverage
         cov = self.coverage()
-        sensor.is_alive = False
+        sensor.deactivate()
         
         if self.coverage() == cov:
             return True
 
-        sensor.is_alive = True
+        sensor.activate()
         return False
 
         # if a point is not otherwise covered, sensor is not redundant
@@ -70,31 +75,111 @@ class SensorNetwork:
         #        sensor.is_active = True
         #        return False
 
-    def reset(self):
+    def reset(self, is_active):
         for sensor in self.sensors:
-            sensor.is_active = True
+            sensor.is_active = is_active
+            sensor.e = self.init_e
+            sensor.is_alive = True
+
+        self.redundant_sensors = []
+        self.a_sensors = []
 
     def round(self):
-        for sensor in self.sensors:
+        for sensor in self.active_sensors():
             sensor.e -= 1
 
             if sensor.e == 0:
-                sensor.is_active = False
+                sensor.kill()
+
+    def alive_sensors(self):
+        return [s for s in self.sensors if s.is_alive]
 
     def active_sensors(self):
-        return [s for s in self.sensors if s.is_active]
+        return [s for s in self.sensors if s.is_active and s.is_alive]
+
+    ###########################
+    #                         #
+    #  redundancy algorithms  #
+    #                         #
+    ###########################
 
     def all_active(self):
-        return self.sensors
+        self.round()
+        return self.active_sensors()
 
     def random_bottom_up(self):
-        sensor = choice(self.sensors)
-        points_covered = [(x, y) for x, y in self.find_intersections()
-                if sensor.contains_point(x, y)]
+        self.round()
 
-        for x, y in points_covered:
-            if not self.is_covered(x, y, True):
+        # select a random sensor among those that are alive and not active
+        random_set = list(set(self.alive_sensors()) - set(self.active_sensors()))
+        
+        # if all sensosrs are active, return
+        if not random_set:
+            return self.active_sensors()
 
+        sensor = random.choice(random_set)
+
+        cov = self.coverage()
+        sensor.activate()
+        
+        if self.coverage() == cov:
+            sensor.deactivate()
+
+        #all_intersections = self.find_intersections(self.sensors)
+        #active_intersections = self.find_intersections(self.active_sensors())
+
+        #for x, y in list(set(all_intersections) - set(active_intersections)):
+        #    if sensor.contains_point(x, y):
+        #        sensor.activate()
+        #        break
+
+        return self.active_sensors()
 
     def random_top_down(self):
-        return list(filter(lambda s: not self.is_redundant(s), self.sensors))
+        self.round()
+
+        active_sensors = self.active_sensors()
+
+        # if all sensors are inactive, return
+        if not active_sensors:
+            for s in self.redundant_sensors:
+                s.activate()
+
+            return self.alive_sensors()
+
+        sensor = random.choice(active_sensors)
+
+        if self.is_redundant(sensor):
+            self.redundant_sensors.append(sensor)
+            sensor.deactivate()
+
+        return self.active_sensors()
+
+    def init_greedy(self):
+        active_sensors = self.active_sensors()
+        active_intersections = self.find_intersections(active_sensors)
+        self.a_sensors = sorted(active_sensors, key=lambda s: s.points_contained(active_intersections))
+
+    def greedy_top_down(self):
+        self.round()
+
+        # sort active sensors by most intersection points
+
+        # filter out sensors we've already determined to not be redundant
+
+        if not self.active_sensors():
+            for s in self.redundant_sensors:
+                s.activate()
+
+            return self.alive_sensors()
+
+        if not self.a_sensors:
+            return self.active_sensors()
+
+        sensor = self.a_sensors.pop()
+
+        if self.is_redundant(sensor):
+            self.redundant_sensors.append(sensor)
+            sensor.deactivate()
+
+        return self.active_sensors()
